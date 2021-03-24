012edc0b5623a98d40b3ff17e490e9830d9b5b33
coderprasukj <prasukjain0047@gmail.com>
2021-03-24 17:15:42 +0530

    Changes in NavBar

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <nav className="navbar navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">Graph Visualizer</a>
-      <a className="github-button" href="https://github.com/awasthishubh/graph-visualizer" data-icon="octicon-star" data-size="large" aria-label="Star awasthishubh/graph-visualizer on GitHub">Star on GitHub</a>
+      <a className="navbar-brand" href="/">
+        Graph Visualizer
+      </a>
+      <a
+        className="github-button"
+        href="https://github.com/itzprasuk/graph-visualizer"
+        data-icon="octicon-star"
+        data-size="large"
+      >
+        Star on GitHub
+      </a>
     </nav>
 
     <div style={{ height: "calc(100vh - 56px)" }}>
       <App />
     </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
